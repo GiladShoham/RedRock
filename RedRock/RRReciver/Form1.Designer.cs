37f7ccc51e5aef573af818abec0446a9a4c0ae9d
Tomer Shohet <tomershohet@gmail.com>
2013-03-12 12:21:07 +0200

    Create all projects and zxing dll

---
+++ RedRock/RRReciver/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RRReciver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateCode = new System.Windows.Forms.Button();
+            this.picOutput = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreateCode
+            // 
+            this.btnCreateCode.Location = new System.Drawing.Point(68, 68);
+            this.btnCreateCode.Name = "btnCreateCode";
+            this.btnCreateCode.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateCode.TabIndex = 0;
+            this.btnCreateCode.Text = "Create QR Code";
+            this.btnCreateCode.UseVisualStyleBackColor = true;
+            this.btnCreateCode.Click += new System.EventHandler(this.btnCreateCode_Click);
+            // 
+            // picOutput
+            // 
+            this.picOutput.Location = new System.Drawing.Point(26, 97);
+            this.picOutput.Name = "picOutput";
+            this.picOutput.Size = new System.Drawing.Size(311, 254);
+            this.picOutput.TabIndex = 1;
+            this.picOutput.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 376);
+            this.Controls.Add(this.picOutput);
+            this.Controls.Add(this.btnCreateCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateCode;
+        private System.Windows.Forms.PictureBox picOutput;
+    }
+}
+
---
+++ RedRock/RRReciver/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using com.google.zxing.qrcode.encoder;
+using com.google.zxing.qrcode;
+using com.google.zxing;
+using com.google.zxing.common;
+
+namespace RRReciver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateCode_Click(object sender, EventArgs e)
+        {
+            QRCodeWriter qcCode = new QRCodeWriter();
+            ByteMatrix btMatrix = qcCode.encode("mayan cohen tomer shohet gilad soham mayan cohen tomer shohet gilad soham mayan cohen tomer shohet gilad soham ", BarcodeFormat.QR_CODE, 200, 200);
+            Bitmap bmpOutput = btMatrix.ToBitmap();
+
+            picOutput.Image = bmpOutput;
+
+        }
+    }
+}
