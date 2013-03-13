980fbfbc7b935d123792c65ad9ebf2b71fb34241
mcyuune <mayancohen23@gmail.com>
2013-03-13 18:51:55 +0200

    dfg

--- RedRock/RRReciver/RecieverForm.cs
+++ RedRock/RRReciver/RecieverForm.cs
@@ -13,90 +13,131 @@ using com.google.zxing.common;
 using com.google.zxing.qrcode;
 using System.Collections;
 using IEC16022Sharp;
+using System.Threading;
+using com.google.zxing.datamatrix.detector;
 
 namespace RedSender
 {
     public partial class RedRock : Form
     {
+        private Boolean stop_executing = false;
+        private FileProcessor mDoAction; 
+
         public RedRock()
         {
             InitializeComponent();
-        }
+
+            this.mDoAction = new FileProcessor();
+            this.mDoAction.stop_executing = false;
+           }
 
         private void btnStartRecieving_Click(object sender, EventArgs e)
+        { /*
+            Thread backgroundThread =
+                new Thread(new ThreadStart(this.mDoAction.ProcessFiles));
+            backgroundThread.Name = "BackgroundThread";
+            backgroundThread.IsBackground = true;
+
+            backgroundThread.Start();
+
+            MessageBox.Show("מבוצעת קליטה");*/
+            this.ProcessFiles();
+        }
+
+        public static void EndProcessAnnounce()
+        {
+            MessageBox.Show("קליטה בוצעה בהצלחה!");
+        }
+
+        private void ProcessFiles()
         {
             string sPicPath = @"C:\Users\Nitzan\Desktop\RedRock\Picture";
 
             string sFileResultType = String.Empty;
 
             Hashtable htAllPictureParts = new Hashtable();
-            int       nAddedPictures = 0;
-            int       nLastPicture   = -1;
-            int       nTotalLenght   = 0;
+            int nAddedPictures = 0;
+            int nLastPicture = -1;
+            int nTotalLenght = 0;
 
-            while (nLastPicture == -1 || nAddedPictures < nLastPicture + 1)
+            while (!stop_executing &&
+                   (nLastPicture == -1 || nAddedPictures < nLastPicture + 1)
+                  )
             {
-                string[] filePaths = Directory.GetFiles(@sPicPath, "*.bmp");
+                string[] filePaths = Directory.GetFiles(@sPicPath, "*.*");
 
                 foreach (string strPic in filePaths)
                 {
-                    // Load the picture
-                    Bitmap image = (Bitmap)Image.FromFile(strPic);
-
-                    ImageConverter converter = new ImageConverter();
-                    Byte[] DecodedArr = (byte[])converter.ConvertTo(image, typeof(byte[]));
-
-                    Byte[] FileIndex = new Byte[8];
-
-                    System.Buffer.BlockCopy(DecodedArr, 0, FileIndex, 0, 8);
-
-                    string sDecodedPicture = System.Text.Encoding.ASCII.GetString(FileIndex);
-
-                    // Split the decoded string to the picture and picture index
-                    int nSpace = sDecodedPicture.IndexOf(' ');
-                    string sPicIndex = sDecodedPicture.Substring(0, nSpace);
-                    string sPicture = sDecodedPicture.Remove(0, nSpace + 1);
-
-                    // If it is the last file - it will have *GIF* ending
-                    int nStarIndex = sPicIndex.IndexOf('*');
-
-                    if (nStarIndex != -1)
+                  /*  if (!this.stop_executing &&
+                        ImageExtensions.Contains(Path.GetExtension(strPic).ToUpperInvariant()))*/
                     {
-                        sFileResultType = sPicIndex.Substring(nStarIndex + 1, sPicIndex.Length - nStarIndex - 1);
-                        sPicIndex = sPicIndex.Substring(0, nStarIndex);
-                        nLastPicture = int.Parse(sPicIndex);
+                        // Load the picture
+                        Bitmap image = (Bitmap)Image.FromFile(strPic);
+
+                        /*ImageConverter converter = new ImageConverter();
+                        Byte[] DecodedArr = (byte[])converter.ConvertTo(image, typeof(byte[]));
+
+                        Byte[] FileIndex = new Byte[8];
+
+                        System.Buffer.BlockCopy(DecodedArr, 0, FileIndex, 0, 8);
+
+                        string sDecodedPicture = System.Text.Encoding.ASCII.GetString(FileIndex);
+                        */
+
+                        string sDecodedPicture = this.QRDecode(image, new QRCodeReader());
+
+                        // Split the decoded string to the picture and picture index
+                        int nSpace = sDecodedPicture.IndexOf(' ');
+                        string sPicIndex = sDecodedPicture.Substring(0, nSpace);
+                        string sPicture = sDecodedPicture.Remove(0, nSpace + 1);
+
+                        // If it is the last file - it will have *GIF* ending
+                        int nStarIndex = sPicIndex.IndexOf('*');
+
+                        if (nStarIndex != -1)
+                        {
+                            int nSecondStarIndex = sPicIndex.IndexOf('*', nStarIndex + 1);
+                            sFileResultType = sPicIndex.Substring(nStarIndex + 1, nSecondStarIndex - nStarIndex - 1);
+                            sPicIndex = sPicIndex.Substring(0, nStarIndex);
+                            nLastPicture = int.Parse(sPicIndex);
+                        }
+
+                        int nPicIndex;
+
+                        try
+                        {
+                            nPicIndex = int.Parse(sPicIndex);
+                        }
+                        catch (Exception E)
+                        {
+                            throw new Exception("Picture format not valid");
+                        }
+
+                        if (!htAllPictureParts.ContainsKey(nPicIndex))
+                        {
+                            //Byte[] btPic = Encoding.ASCII.GetBytes(sPicture);
+                            Byte[] btPic = Convert.FromBase64String(sPicture.Trim());
+
+                            htAllPictureParts.Add(nPicIndex, btPic);
+                            ++nAddedPictures;
+
+                            nTotalLenght = nTotalLenght + btPic.Length;
+                        }
+
+                        image.Dispose();
                     }
-
-                    int nPicIndex;
-
-                    try
-                    {
-                        nPicIndex= int.Parse(sPicIndex);
-                    }
-                    catch( Exception E)
-                    {
-                        throw new Exception("Picture format not valid");
-                    }
-
-                    if (!htAllPictureParts.ContainsKey(nPicIndex))
-                    {
-                        Byte[] btPic = Encoding.ASCII.GetBytes(sPicture);
-
-                        File.WriteAllBytes(@"C:\Users\Nitzan\Desktop\RedRock\Result\.GIF", btPic);
-
-                        htAllPictureParts.Add(nPicIndex, btPic);
-                        ++nAddedPictures;
-
-                        nTotalLenght = nTotalLenght + btPic.Length;
-                    }
-
-                    image.Dispose();
+                    // delete the picture when closes
+                    //        System.IO.File.Delete(sPicPath);
                 }
-                // delete the picture when closes
-        //        System.IO.File.Delete(sPicPath);
             }
-
-            this.CreateResultFile(htAllPictureParts, nTotalLenght, sFileResultType);
+            if (!stop_executing)
+            {
+                this.CreateResultFile(htAllPictureParts, nTotalLenght, sFileResultType);
+            }
+            else
+            {
+                this.stop_executing = false;
+            }
         }
 
 
@@ -108,12 +149,12 @@ namespace RedSender
             int offset = 0;
             for (int i = 0; i <= htPictures.Count - 1; i++)
             {
-                Byte[] array = (Byte[]) htPictures[i];
+                Byte[] array = (Byte[])htPictures[i];
                 System.Buffer.BlockCopy(array, 0, rv, offset, array.Length);
                 offset += array.Length;
             }
 
-            using (FileStream fs = File.Create(@"C:\Users\Nitzan\Desktop\RedRock\Result\Result." + sFileType))
+            using (FileStream fs = File.Create(@"C:\Users\Nitzan\Desktop\RedRock\Result\Result" + sFileType))
             {
                 foreach (Byte b in rv)
                 {
@@ -126,27 +167,51 @@ namespace RedSender
 
         private string QRDecode(Bitmap image, Reader decoder)
         {
-            /*
-            MemoryStream ms = new MemoryStream();
-            image.Save(ms,System.Drawing.Imaging.ImageFormat.Bmp);
 
-            DataMatrix dmDecoder = new DataMatrix(ms.ToArray(), image.Width, image.Height, EncodingType.Binary);
+            string sdecodedString = string.Empty;
 
-            return dmDecoder.HexPbm;
-             * */
+                LuminanceSource source = new RGBLuminanceSource(image, image.Width, image.Height);
+                var rgb = new HybridBinarizer(source);
+                var binBitmap = new BinaryBitmap(rgb);
+                
+                BitMatrix bm = binBitmap.BlackMatrix;
+
+				
+		
+
+
+
+                 /*   
+                Detector detector = new Detector(bm);
+                DetectorResult result = detector.detect();
+
+                string retStr = "Found at points ";
+                BitMatrix QRImageData = result.Bits;
+                  */
+/*
+                image m = QRImageData;
+                LuminanceSource source = new RGBLuminanceSource(QRImageData, image.Width, image.Height);
+                var rgb = new HybridBinarizer(source);
+                var binBitmap = new BinaryBitmap(rgb);
+
+
+                BinaryBitmap m = new BinaryBitmap(
+
+                sdecodedString = decoder.decode(().Text;*/
+  
+
+
             
-
-            
-            var rgb = new RGBLuminanceSource(image, image.Width, image.Height);
-            var hybrid = new HybridBinarizer(rgb);
-
-            BinaryBitmap binBitmap = new BinaryBitmap(hybrid);
-           // string sdecodedString = decoder.decode(binBitmap, null).RawBytes;
             //Byte[] result = decoder.decode(binBitmap, null).;
 
             //return null;
 
-            return null;
+            return sdecodedString;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.mDoAction.stop_executing = true;
         }
     }
 }
