4160d655251c164da5884710965a5e8501b3b9c9
mcyuune <mayancohen23@gmail.com>
2013-03-13 08:23:13 +0200

    receieveDecoder first commit

--- RedRock/RedSender/Form1.Designer.cs
+++ RedRock/RedSender/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RedSender
 {
-    partial class Form1
+    partial class RedRock
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStartRecieving = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartRecieving
+            // 
+            this.btnStartRecieving.Location = new System.Drawing.Point(104, 46);
+            this.btnStartRecieving.Name = "btnStartRecieving";
+            this.btnStartRecieving.Size = new System.Drawing.Size(75, 23);
+            this.btnStartRecieving.TabIndex = 0;
+            this.btnStartRecieving.Text = "בצע פענוח";
+            this.btnStartRecieving.UseVisualStyleBackColor = true;
+            this.btnStartRecieving.Click += new System.EventHandler(this.btnStartRecieving_Click);
+            // 
+            // RedRock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 126);
+            this.Controls.Add(this.btnStartRecieving);
+            this.Name = "RedRock";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "הסלע האדום";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStartRecieving;
+
     }
 }
 
