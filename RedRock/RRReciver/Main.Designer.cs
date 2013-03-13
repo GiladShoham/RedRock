d2c2dee288579bd9d93b40e0cde052f6aabacd39
Tomer Shohet <tomershohet@gmail.com>
2013-03-13 09:38:28 +0200

    Add main at sender

---
+++ RedRock/RRReciver/Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RRReciver
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateCode = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCreateCode
+            // 
+            this.btnCreateCode.Location = new System.Drawing.Point(26, 39);
+            this.btnCreateCode.Name = "btnCreateCode";
+            this.btnCreateCode.Size = new System.Drawing.Size(311, 23);
+            this.btnCreateCode.TabIndex = 0;
+            this.btnCreateCode.Text = "Create QR Code";
+            this.btnCreateCode.UseVisualStyleBackColor = true;
+            this.btnCreateCode.Click += new System.EventHandler(this.btnCreateCode_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(26, 13);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(311, 20);
+            this.txtFilePath.TabIndex = 2;
+            this.txtFilePath.Text = "D:\\Test\\New\\trol.gif";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 78);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnCreateCode);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateCode;
+        private System.Windows.Forms.TextBox txtFilePath;
+    }
+}
+
---
+++ RedRock/RRReciver/Main.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using com.google.zxing.qrcode.encoder;
+using com.google.zxing.qrcode;
+using com.google.zxing;
+using com.google.zxing.common;
+
+using IEC16022Sharp;
+using System.IO;
+using Gif.Components;
+using System.Drawing.Imaging;
+
+namespace RRReciver
+{
+    public partial class Main : Form
+    {
+        #region Data Members
+
+        // Const Members
+        public const int BYTES_IN_FRAME        = 624;
+        public const int NUM_OF_SEQUENCE_DIGIT = 3;
+        public const int QR_HIGHET             = 144;
+        public const int QR_WIDTH              = 144;
+        private string OUTPUT_FOLDER_BASE = @"D:\Tomer\Studies\Dropbox\RedRock\QRSample\New\";
+        
+        // Data Member
+        private string OUTPUT_FOLDER_BMPS;
+        private string OUTPUT_FOLDER_TXT;
+        private string OUTPUT_FOLDER_GIF;
+        private string m_strCurrFileLocation = string.Empty;
+        private string m_strFileName         = string.Empty;
+        private int    m_nCurrTestNum        = 0;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event Methods
+
+        private void CreateFolders()
+        {
+            string strTestCounter = (Directory.GetDirectories(OUTPUT_FOLDER_BASE).Length + 1) + @"\";
+
+            Directory.CreateDirectory(OUTPUT_FOLDER_BASE + strTestCounter);
+            
+            File.Copy(m_strCurrFileLocation, OUTPUT_FOLDER_BASE + strTestCounter + Path.GetFileName(m_strCurrFileLocation));
+            m_strCurrFileLocation = OUTPUT_FOLDER_BASE + strTestCounter + Path.GetFileName(m_strCurrFileLocation);
+
+            OUTPUT_FOLDER_BMPS = OUTPUT_FOLDER_BASE + strTestCounter + "BMP\\";
+            OUTPUT_FOLDER_TXT = OUTPUT_FOLDER_BASE + strTestCounter + "TXT\\";
+            OUTPUT_FOLDER_GIF = OUTPUT_FOLDER_BASE + strTestCounter + "GIF\\";
+
+            Directory.CreateDirectory(OUTPUT_FOLDER_BMPS);
+            Directory.CreateDirectory(OUTPUT_FOLDER_GIF);
+            Directory.CreateDirectory(OUTPUT_FOLDER_TXT);
+        }
+
+        private void btnCreateCode_Click(object sender, EventArgs e)
+        {
+            // Set the curr file details to the data members
+            m_strCurrFileLocation = this.txtFilePath.Text;
+            m_strFileName = Path.GetFileName(m_strCurrFileLocation);
+
+            // Create Folders
+            CreateFolders();
+
+            //string strZipFile = this.Comprass(m_strCurrFileLocation);
+            string strZipFile = m_strCurrFileLocation;
+            byte[] btArray = File.ReadAllBytes(strZipFile);
+
+            //String strFullBitmap = System.Text.Encoding.UTF8.GetString(btArray);
+
+            int NumOfFrame = btArray.Length / (BYTES_IN_FRAME + NUM_OF_SEQUENCE_DIGIT);
+
+            Bitmap[] arrbpmOutput = new Bitmap[NumOfFrame];
+
+            QRCodeWriter qcCode = new QRCodeWriter();
+            string strLastStringAddon = string.Empty;
+            for (int nCurrFrameNumber = 0; nCurrFrameNumber < NumOfFrame; nCurrFrameNumber++ )
+            {
+                // TODO: Restore this
+                /*
+                string strOneFrame;
+
+                // Check if it is the last string or not
+                if (nCurrFrameNumber != NumOfFrame - 1)
+                {
+                    // If it is not the last string - then it cut the string from the right point in the right size
+                    strOneFrame = strFullBitmap.Substring(nCurrFrameNumber * BYTES_IN_FRAME, BYTES_IN_FRAME);
+                }
+                else
+                {
+                    // If it is the last string - cut from the right point till the end
+                    strOneFrame = strFullBitmap.Substring(nCurrFrameNumber * BYTES_IN_FRAME);
+                    strLastStringAddon = "*" + Path.GetExtension(m_strFileName) + "*";
+                }
+                 
+
+                // Add the number (with the 0 if it is only one digit) to the start of the image
+                strOneFrame = string.Format("{0:00}", nCurrFrameNumber) + strLastStringAddon + " " + strOneFrame;
+                */
+
+                // Encode the string to QRCode
+                //ByteMatrix btMatrix = qcCode.encode(strOneFrame, BarcodeFormat.DATAMATRIX, QR_WIDTH, QR_HIGHET);
+                byte[] btBytesCurrFrame = new byte[BYTES_IN_FRAME];
+                for (int i = 0; i < BYTES_IN_FRAME; i++)
+			    {
+                    btBytesCurrFrame[i] = btArray[i];
+			    }
+                
+                
+
+                DataMatrix dm = new DataMatrix(btBytesCurrFrame, QR_WIDTH, QR_HIGHET, EncodingType.Binary);
+                dm.Image.Save(OUTPUT_FOLDER_BMPS + nCurrFrameNumber + ".bmp", ImageFormat.Bmp);
+
+                
+                
+                //arrbpmOutput[nCurrFrameNumber] = btMatrix.ToBitmap();
+            //    File.CreateText(OUTPUT_FOLDER_BMPS + nCurrFrameNumber + ".txt");
+                //File.WriteAllText(OUTPUT_FOLDER_TXT + nCurrFrameNumber + ".txt", strOneFrame);
+
+                //arrbpmOutput[nCurrFrameNumber].Save(OUTPUT_FOLDER_BMPS + nCurrFrameNumber + ".bmp");
+
+                this.CreateAnimitedGif();
+            }
+        }
+
+        private string Comprass(string startPath)
+        {
+            string strZipPath = GZipDecoder.Compress(new FileInfo(startPath));
+
+            return (strZipPath);
+        }
+
+        private void CreateAnimitedGif()
+        {
+            /* create Gif */
+            // you should replace filepath
+            string[] imageFilePaths = Directory.GetFiles(OUTPUT_FOLDER_BMPS);
+
+            String outputFilePath = OUTPUT_FOLDER_GIF + "output.gif";
+            AnimatedGifEncoder ege = new AnimatedGifEncoder();
+            ege.Start(outputFilePath);
+            ege.SetDelay(500);
+            //-1:no repeat,0:always repeat
+            ege.SetRepeat(0);
+            for (int i = 0, count = imageFilePaths.Length; i < count; i++)
+            {
+                ege.AddFrame(Image.FromFile(imageFilePaths[i]));
+            }
+            ege.Finish();
+        }
+
+        #endregion
+    }
+}
